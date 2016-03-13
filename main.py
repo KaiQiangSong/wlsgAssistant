333c767d1c914ec278dd8e4e1cccfbaf959ab30e
SkywalkerQ <kqsong2014@gmail.com>
2016-03-14 00:47:47 +0800

    Add Socket Default timeout

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 # coding=utf-8
-import urllib,urllib2,cookielib,json;
-import threading,time,random;
-import login;
-import operation;
-import transport;
+
+# Usage of Python Lib
+import socket,urllib,urllib2,cookielib,json;
+import threading,time,random,ssl;
 import time;
 import string;
+
+# Usage of Some Basic Functions
+import login;
+import operation;
 import tools;
+
+# Usage of Some Functional Parts
+import transport;
 import open_box;
 import trainSoldiers;
 import accelerate;
 import Building;
 
+# Set Default Timeout
+default_timeout = 10;
+socket.setdefaulttimeout(default_timeout);
 
+# Set SSL
+
+# Main Program
 transport.main_program();
